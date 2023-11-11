35a315865efaea4fceb761fe416c505152d47e94
Ueda01 <ppueda120@gmail.com>
2023-11-11 19:46:44 -0300

    sidebar

---
+++ js/script.js
@@ -0,0 +1,23 @@
+// sidebar
+var menuItem = document.querySelectorAll('.item-menu');
+
+// deixa selecionado o item que eu cliquei na sidebar
+function selectLink() {
+    menuItem.forEach((item) =>
+            item.classList.remove('ativo')
+        );
+        this.classList.add('ativo');
+};
+
+menuItem.forEach((item)=>
+    item.addEventListener('click', selectLink)
+);
+
+//icone para abrir a sidebar
+
+var btnExp = document.querySelector('#btn-exp');
+var menuSide = document.querySelector('.menu-lateral');
+
+btnExp.addEventListener('click', function(){
+    menuSide.classList.toggle('expandir')
+})
