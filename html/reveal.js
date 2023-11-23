c836002f9ab7e1676450053653d715693e9664df
Ueda01 <ppueda120@gmail.com>
2023-11-23 11:18:16 -0300

    promoções com efeito

---
+++ html/reveal.js
@@ -0,0 +1,33 @@
+// ESTE ARQUIVO É APENAS PARA CRIAR E CONTROLAR OS EFEITOS DE ROLAGEM DO SITE
+
+window.revelar = ScrollReveal({reset:true});
+
+// BOX PROMOÇÃO 1
+
+revelar.reveal('.efeito-promo1', 
+{
+    duration: 2000,
+    distance: "120px",
+    origin: "left"
+});
+
+revelar.reveal('.efeito-promo2', 
+{
+    duration: 2000,
+    distance: "120px",
+    origin: "right"
+});
+
+revelar.reveal('.efeito-promo3', 
+{
+    duration: 2000,
+    distance: "120px",
+    origin: "left"
+});
+
+revelar.reveal('.efeito-promo4', 
+{
+    duration: 2000,
+    distance: "120px",
+    origin: "right"
+});
