028e03a7c4a48d00443f123f6959b8f69c12b8ac
Ueda01 <ppueda120@gmail.com>
2023-12-01 11:34:56 -0300

    arrumei index e promoções, e mudei fontes

--- js/promocoes.js
+++ js/promocoes.js
@@ -5,10 +5,45 @@ const flexRestaurantes = document.querySelector(".flex-restaurantes");
 
 // CRIANDO A FUNÇÃO PARA MOVER PARA ESQUERDA
 const handleLeftClick = (e) => {
-    flexRestaurantes.scrollLeft -= flexRestaurantes.offsetWidth;
+    let tamParaRolar = flexRestaurantes.offsetWidth - 800;
+    flexRestaurantes.scrollLeft -= tamParaRolar;
 };
 
 // CRIANDO A FUNÇÃO PARA MOVER PARA DIREITA
 const handleRightClick = (e) => {
-    flexRestaurantes.scrollLeft += flexRestaurantes.offsetWidth;
+    let tamParaRolar = flexRestaurantes.offsetWidth - 800;
+    flexRestaurantes.scrollLeft += tamParaRolar;
 };
+
+// sidebar
+var menuItem = document.querySelectorAll('.item-menu');
+
+// deixa selecionado o item que eu cliquei na sidebar
+function selectLink() {
+    menuItem.forEach((item) =>
+        item.classList.remove('ativo')
+    );
+    this.classList.add('ativo');
+};
+
+menuItem.forEach((item) =>
+    item.addEventListener('click', selectLink)
+);
+
+//icone para abrir a sidebar
+
+var btnExp1 = document.querySelector('#btn-exp1');
+var btnExp = document.querySelector('#btn-exp');
+var menuSide = document.querySelector('.menu-lateral');
+
+btnExp1.addEventListener('click', function () {
+    menuSide.classList.toggle('expandir')
+})
+
+btnExp.addEventListener('click', function () {
+    menuSide.classList.toggle('expandir')
+})
+
+function goHome(){
+    window.location = 'index.html';
+}
