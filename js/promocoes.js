767591686889e21c124800e5646bb16c330d470d
Ueda01 <ppueda120@gmail.com>
2023-12-02 01:07:40 -0300

    promoções finalmente pronta, mas o rodapé bagunçou a pag de pontos, dps da uma olhada pires

--- js/promocoes.js
+++ js/promocoes.js
@@ -15,35 +15,7 @@ const handleRightClick = (e) => {
     flexRestaurantes.scrollLeft += tamParaRolar;
 };
 
-// sidebar
-var menuItem = document.querySelectorAll('.item-menu');
-
-// deixa selecionado o item que eu cliquei na sidebar
-function selectLink() {
-    menuItem.forEach((item) =>
-        item.classList.remove('ativo')
-    );
-    this.classList.add('ativo');
-};
-
-menuItem.forEach((item) =>
-    item.addEventListener('click', selectLink)
-);
-
-//icone para abrir a sidebar
-
-var btnExp1 = document.querySelector('#btn-exp1');
-var btnExp = document.querySelector('#btn-exp');
-var menuSide = document.querySelector('.menu-lateral');
-
-btnExp1.addEventListener('click', function () {
-    menuSide.classList.toggle('expandir')
-})
-
-btnExp.addEventListener('click', function () {
-    menuSide.classList.toggle('expandir')
-})
-
+// FUNÇÃO PARA LEVAR PARA A PAGINA INICIAL
 function goHome(){
     window.location = 'index.html';
 }
